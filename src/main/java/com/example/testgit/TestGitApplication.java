93f40294ab06201f481a767907032add3c9137ea
lilinjun-llj <1652158644@qq.com>
2024-10-16 11:33:17 +0800

    add

---
+++ src/main/java/com/example/testgit/TestGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.testgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitApplication {
+	//dsfsf
+	public static void main(String[] args) {
+		SpringApplication.run(TestGitApplication.class, args);
+	}
+
+}
